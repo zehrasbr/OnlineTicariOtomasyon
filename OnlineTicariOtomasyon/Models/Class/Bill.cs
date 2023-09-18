fe0d51198ca584e55f083e3c9ee83079c2028d14
zehrasbr <zehraisbr@gmail.com>
2023-09-18 22:44:19 +0300

    added department bill class

---
+++ OnlineTicariOtomasyon/Models/Class/Bill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Class
+{
+    public class Bill
+    {
+        public int BillID { get; set; }
+        public string BillSerialNo { get; set; }
+        public string BillSequenceNo { get; set; }
+        public DateTime Date { get; set; }
+        public string TaxAdministration { get; set; }
+        public DateTime Clock { get; set; }
+        public string Deliverer { get; set; }
+        public string DeliveryArea { get; set; }
+    }
+}
---
+++ OnlineTicariOtomasyon/Models/Class/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Class
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
