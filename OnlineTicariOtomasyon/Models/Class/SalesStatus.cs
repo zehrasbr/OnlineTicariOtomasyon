ec4a2634990d41a02eb96c129714522861f779d0
zehrasbr <zehraisbr@gmail.com>
2023-09-18 22:56:25 +0300

    added salesstatus class

---
+++ OnlineTicariOtomasyon/Models/Class/SalesStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Class
+{
+    public class SalesStatus
+    {
+        [Key]
+        public int SalesStatusID { get; set; }
+        //ürün
+        //cari
+        //personel
+        public DateTime Date { get; set; }
+        public int Piece { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
