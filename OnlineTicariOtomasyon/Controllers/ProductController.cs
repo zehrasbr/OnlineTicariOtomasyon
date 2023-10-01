c6559c95d7f81c811cbdef3b70e488c33b3913d8
zehrasbr <zehraisbr@gmail.com>
2023-10-01 22:34:34 +0300

    product add finished

--- OnlineTicariOtomasyon/Controllers/ProductController.cs
+++ OnlineTicariOtomasyon/Controllers/ProductController.cs
@@ -12,12 +12,18 @@ namespace OnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Products.ToList();
+            var urunler = c.Products.Where(x=>x.Status==true).ToList();
             return View(urunler);
         }
         [HttpGet]
         public ActionResult YeniUrun()
         {
+            List<SelectListItem> deger1 = (from x in c.Categories.ToList() select new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value=x.CategoryID.ToString()
+            }).ToList();
+             ViewBag.dgr1 = deger1;
             return View();
         }
         [HttpPost]
@@ -26,6 +32,17 @@ namespace OnlineTicariOtomasyon.Controllers
             c.Products.Add(p);
             c.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public ActionResult UrunSil(int id)
+        {
+            var deger = c.Products.Find(id);
+            deger.Status = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir()
+        {
+            return View();
         }
     }
 }
