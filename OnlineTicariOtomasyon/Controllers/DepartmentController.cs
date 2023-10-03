9b6e4d3206a7e2691dfa48fa9196fdb6d5286ad9
zehrasbr <zehraisbr@gmail.com>
2023-10-03 20:53:03 +0300

    department

---
+++ OnlineTicariOtomasyon/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using OnlineTicariOtomasyon.Models.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Class;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Departments.ToList();
+            return View(degerler);
+        }
+    }
+}
