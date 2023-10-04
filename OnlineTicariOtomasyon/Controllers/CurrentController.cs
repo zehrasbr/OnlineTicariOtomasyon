2509319174b415fece5f5f23388affabe994fa7c
zehrasbr <zehraisbr@gmail.com>
2023-10-04 20:22:34 +0300

    current list

---
+++ OnlineTicariOtomasyon/Controllers/CurrentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Class;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CurrentController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Currents.ToList();
+            return View(degerler);
+        }
+    }
+}
