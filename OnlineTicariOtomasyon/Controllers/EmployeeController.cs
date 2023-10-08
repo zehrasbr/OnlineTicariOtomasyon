8294278ebec957fe13c16b3a55a65941775dc596
zehrasbr <zehraisbr@gmail.com>
2023-10-08 14:34:24 +0300

    employee add

--- OnlineTicariOtomasyon/Controllers/EmployeeController.cs
+++ OnlineTicariOtomasyon/Controllers/EmployeeController.cs
@@ -15,5 +15,24 @@ namespace OnlineTicariOtomasyon.Controllers
             var degerler = c.Employees.ToList();
             return View(degerler);
         }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            List<SelectListItem> deger1 = (from x in c.Departments.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmentName,
+                                               Value = x.DepartmentID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(Employee p)
+        {
+            c.Employees.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
