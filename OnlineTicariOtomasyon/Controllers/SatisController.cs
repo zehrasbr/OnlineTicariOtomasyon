cfb3d2c6176b45299634e52153f966acd6cc6dd0
zehrasbr <zehraisbr@gmail.com>
2023-10-13 14:13:35 +0300

    sales status update

--- OnlineTicariOtomasyon/Controllers/SatisController.cs
+++ OnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -49,5 +49,31 @@ namespace OnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Products.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductID.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger2 = (from x in c.Currents.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CurrentName + " " + x.CurrentSurname,
+                                               Value = x.CurrentID.ToString()
+                                           }).ToList();
+            List<SelectListItem> deger3 = (from x in c.Employees.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.EmployeeName + " " + x.EmployeeSurname,
+                                               Value = x.EmployeeID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            var deger = c.SalesStatuses.Find(id);
+            return View("SatisGetir", deger);
+        }
     }
 }
