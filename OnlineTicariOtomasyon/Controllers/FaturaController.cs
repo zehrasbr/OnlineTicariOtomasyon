9c12c8478c9cfadb18e96ca5ab6146f047bc1cb0
zehrasbr <zehraisbr@gmail.com>
2023-12-27 20:50:04 +0300

    bill

---
+++ OnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Class;
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Bills.ToList();
+            return View(liste);
+        }
+    }
+}
