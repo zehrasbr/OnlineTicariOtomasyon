f322aaa95ad5bb27aa74b6c06d2303756a537adc
zehrasbr <zehraisbr@gmail.com>
2023-12-28 21:07:02 +0300

    statistic

---
+++ OnlineTicariOtomasyon/Controllers/istatistikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class istatistikController : Controller
+    {
+        // GET: istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
