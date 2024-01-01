6dc8c1136f7607ddbb7d994f5138d27e798dfc85
zehrasbr <zehraisbr@gmail.com>
2024-01-02 00:14:34 +0300

    update

--- OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
+++ OnlineTicariOtomasyon/Controllers/UrunDetayController.cs
@@ -11,8 +11,11 @@ namespace OnlineTicariOtomasyon.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var degerler = c.Products.Where(x=>x.ProductID == 1).ToList();
-            return View(degerler);
+            Class1 cs = new Class1();
+            //var degerler = c.Products.Where(x=>x.ProductID == 1).ToList();
+            cs.Deger1 = c.Products.Where(x=>x.ProductID == 1).ToList();
+            cs.Deger2 = c.Detays.Where(y => y.DetayID == 1).ToList();
+            return View(cs);
         }
     }
 }
---
+++ OnlineTicariOtomasyon/Models/Class/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Class
+{
+    public class Class1
+    {
+        public IEnumerable<Product> Deger1 { get; set; }
+        public IEnumerable<Detay> Deger2 { get; set; }
+
+    }
+}
