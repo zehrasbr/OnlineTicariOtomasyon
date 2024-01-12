d10282abca1d8fd31da6034162302113d5540b66
zehrasbr <zehraisbr@gmail.com>
2024-01-12 13:40:14 +0300

    .

--- OnlineTicariOtomasyon/Controllers/YapilacakController.cs
+++ OnlineTicariOtomasyon/Controllers/YapilacakController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OnlineTicariOtomasyon.Models.Class;
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class YapilacakController : Controller
     {
-        // GET: Yapilacak
+        Context c = new Context();
         public ActionResult Index()
         {
+            var deger1 = c.Currents.Count().ToString();
+            ViewBag.d1 = deger1;
+            var deger2 = c.Products.Count().ToString();
+            ViewBag.d2 = deger2;
+            var deger3 = c.Categories.Count().ToString();
+            ViewBag.d3 = deger3;
+            //var deger4 = c.Currents.Distinct().Count(x=>x.).ToString();
+           // ViewBag.d4 = deger4;
             return View();
         }
     }
