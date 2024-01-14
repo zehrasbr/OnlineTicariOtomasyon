4b9dd38b0f680ced59f04732cb78e22c2cc2356a
zehrasbr <zehraisbr@gmail.com>
2024-01-14 13:16:41 +0300

    .

---
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
