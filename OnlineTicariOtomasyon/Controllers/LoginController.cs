672c783dab8708e2850f87671807723c63228d25
zehrasbr <zehraisbr@gmail.com>
2024-01-14 16:40:45 +0300

    login

--- OnlineTicariOtomasyon/Controllers/LoginController.cs
+++ OnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
@@ -25,6 +26,26 @@ namespace OnlineTicariOtomasyon.Controllers
             c.Currents.Add(p);
             c.SaveChanges();
             return PartialView();
+        }
+        [HttpGet]
+        public ActionResult CariLogin1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CariLogin1(Current p)
+        {
+            var bilgiler = c.Currents.FirstOrDefault(x => x.CurrentMail == p.CurrentMail && x.CurrentPassword == p.CurrentPassword);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.CurrentMail, false);
+                Session["CurrentMail"] = bilgiler.CurrentMail.ToString();
+                return RedirectToAction("Index", "CariPanel");
+            }
+            else
+            {
+                return RedirectToAction("Index","Login");
+            }
         }
     }
 }
