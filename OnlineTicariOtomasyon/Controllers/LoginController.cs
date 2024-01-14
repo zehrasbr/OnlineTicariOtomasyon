b410ce9d45809d8e00ccb69dd4131db4c17a8cab
zehrasbr <zehraisbr@gmail.com>
2024-01-14 12:46:48 +0300

    register

--- OnlineTicariOtomasyon/Controllers/LoginController.cs
+++ OnlineTicariOtomasyon/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineTicariOtomasyon.Models.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,13 +9,21 @@ namespace OnlineTicariOtomasyon.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult Partial1()
         {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Partial1(Current p)
+        {
+            c.Currents.Add(p);
+            c.SaveChanges();
             return PartialView();
         }
     }
