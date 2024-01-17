bdd4d4afe91792f7ba3c06d12b162abd6dcf24ed
zehrasbr <zehraisbr@gmail.com>
2024-01-17 22:34:33 +0300

    .

--- OnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ OnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -18,5 +18,12 @@ namespace OnlineTicariOtomasyon.Controllers
             ViewBag.m = mail;
             return View(degerler);
         }
+        public ActionResult Siparislerim()
+        {
+            var mail = (string)Session["CurrentMail"];
+            var id = c.Currents.Where(x => x.CurrentMail == mail.ToString()).Select(y => y.CurrentID).FirstOrDefault();
+            var degerler = c.SalesStatuses.Where(x => x.Currentid == id).ToList();
+            return View(degerler);
+        }
     }
 }
