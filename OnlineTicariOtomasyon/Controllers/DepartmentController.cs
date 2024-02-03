bf1ed3e923bfa441539faea08c742b8e202fe2d8
zehrasbr <zehraisbr@gmail.com>
2024-02-03 16:16:15 +0300

    .

--- OnlineTicariOtomasyon/Controllers/DepartmentController.cs
+++ OnlineTicariOtomasyon/Controllers/DepartmentController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using OnlineTicariOtomasyon.Models.Class;
 
 namespace OnlineTicariOtomasyon.Controllers
 {
